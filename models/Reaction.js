7be18cf47db4ec6885a33973396883c3692000c2
Daniela Leal <falfada@gmail.com>
2024-07-08 19:34:05 -0500

    Created models and connection to database

---
+++ models/Reaction.js
@@ -0,0 +1,17 @@
+const { Schema } = require("mongoose");
+
+const reactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new mongoose.Types.ObjectId(),
+  },
+  reactionBody: { type: String, required: true, maxLength: 280 },
+  username: { type: String, required: true },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAt) => new Date(createdAt).toDateString(),
+  },
+});
+
+module.exports = reactionSchema;
---
+++ models/Thought.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require("mongoose");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: { type: String, required: true, minLength: 1, maxLength: 280 },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAt) => new Date(createdAt).toDateString(),
+    },
+    username: { type: String, required: true },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getter: true,
+    },
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.ractions.length;
+});
+
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
